e341c90f0535777f49c78be67d771838a9989e26
Ankit Bansal <ankit.bansal@sevenx.io>
2022-02-07 22:28:35 +0530

    Sprial matrix program

--- SprialProgram.java
+++ SprialProgram.java
@@ -8,42 +8,52 @@ public class SprialProgram {
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter value for sprial matrix : ");
-		int value = sc.nextInt();
+		int input = sc.nextInt();
 		
-		int matrix[][] = new int[value][value];
+		int matrix[][] = new int[input][input];
 		
-		int counter = 1;
-		int left = 0;
-		int right = value-1;
-		int start = 1;
+		int currentNumber = 1;
+		int startRow = 0;
+		int startColumn = 0;
+		int endRow = input-1;
+		int endColumn = input-1;
 		
-		while(start<=value/2) {
+		while( (startRow <= endRow) && (startColumn <= endColumn) ) {
 			
-			for(int i = left;i<=right;i++) {
-				matrix[left][i] = counter++;
+			//Left to right
+			for(int i = startColumn; i <= endColumn; i++) {
+				matrix[startRow][i] = currentNumber++;
 			}
+			startRow++;
 			
-			for(int i = left+1;i<=right;i++) {
-				matrix[i][right] = counter++;
-			}
-			
-			for(int i = right-1;i>=left;i--) {
-				matrix[right][i] = counter++;
-			}
-			
-			for(int i = right-1;i>=left+1;i--) {
-				matrix[i][left] = counter++;
-			}
-			
-			left++;
-			right--;
-			start++;
+			//top to bottom
+        	for(int i = startRow; i <= endRow; i++) {
+               	matrix[i][endColumn]=currentNumber++;
+        	}
+            endColumn--;
+        	
+        	//right to left
+        	for(int i = endColumn; i >= startColumn; i--) {
+                
+        		matrix[endRow][i]=currentNumber;
+        		currentNumber++;
+        	}
+        	endRow--;
+        	
+        	//bottom to top
+        	for(int i = endRow; i >= startRow; i--) {
+        		matrix[i][startColumn] = currentNumber++;
+        	}
+        	startColumn++;
+        	
 		}
 		
-		for(int i = 0;i<value;i++) {
-			for(int j = 0;j<value;j++) {
-				System.out.printf("%4d",matrix[i][j]);
+		for(int i = 0;i<input;i++) {
+			
+			for(int j = 0; j < input; j++) {
+				System.out.printf("%4d", matrix[i][j]);
 			}
+			
 			System.out.println();
 		}
 
